523d673b36b02ad05c2898bab435bf9cb20d1a8e
Johannes Schäfer <johannes.ludwig.schaefer@gmail.com>
2024-03-31 11:49:20 +0200

    proof-reading and filled the error page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'js-portfolio-not-found',
-  standalone: true,
-  imports: [],
-  templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+	selector: 'js-portfolio-not-found',
+	standalone: true,
+	imports: [],
+	templateUrl: './not-found.component.html',
+	styleUrl: './not-found.component.scss',
 })
 export class NotFoundComponent {
-
+	public readonly catImageUrl = 'https://cataas.com/cat';
+	public readonly catImageAlt = 'A random cat';
 }
